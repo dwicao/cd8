bc9f5696ab2c09850e7a31ad07325bbcd7565ccb
dwicao <lutfiandwica@gmail.com>
2017-05-17 15:00:30 +0700

    change MyText to ListView

--- src/containers/Home/components/MyText/index.js
+++ src/containers/Home/components/MyText/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
+  ListView,
   TouchableOpacity,
 } from 'react-native';
+import styles from './index.style';
 
 const MyText = (props) => {
+  const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  const newDataSource = ds.cloneWithRows(props.todos);
+
+  const renderRow = (todo) => {
+    return (
+      <Text style={styles.text}>
+        {todo.text
+      }</Text>
+    );
+  }
+
   return (
-    <View>
-      {
-        props.todos.map((todo, index) => (
-          <Text key={index}>{todo.text}</Text>
-        ))
-      }
+    <View style={styles.container}>
+      <ListView
+        contentContainerStyle={styles.contentContainerStyle}
+        dataSource={newDataSource}
+        renderRow={renderRow}
+      />
     </View>
   );
 }
---
+++ src/containers/Home/components/MyText/index.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  contentContainerStyle: {
+    height: 450,
+    backgroundColor: 'rgba(0,0,0, 0.05)'
+  },
+  text: {
+    margin: 10,
+    backgroundColor: '#ccc',
+  },
+});
